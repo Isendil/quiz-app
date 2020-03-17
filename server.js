ed9f68da03fb35b177a49f9a7b5157a5b803bfa9
Dimitrios Ellinidis <isendil_1@yahoo.com>
2020-03-17 13:40:26 -0400

    commit changes her

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-//Install express server
-const express = require("express");
-const path = require("path");
+// //Install express server
+// const express = require("express");
+// const path = require("path");
 
-const app = express();
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<name-of-app>"));
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + "/dist/<name-of-app>"));
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+// app.get("/*", function(req, res) {
+//   res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+var express = require("express");
+var path = require("path");
+var app = express();
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
+app.listen(process.env.PORT || 3000);
