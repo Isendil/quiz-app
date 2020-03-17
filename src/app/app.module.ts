343d03740483eb65d08c96e2491cbc888ed8ad10
Dimitrios Ellinidis <isendil_1@yahoo.com>
2020-03-17 11:32:49 -0400

    commiting finished 3.8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ResultsComponent } from "./results/results.component";
 import { RouterModule, Routes } from "@angular/router";
 import { WelcomeComponent } from "./welcome/welcome.component";
 import { QuestionsComponent } from "./questions/questions.component";
+import { MatCardModule } from "@angular/material/card";
 
 const appRoutes: Routes = [
   { path: "welcome", component: WelcomeComponent },
@@ -30,7 +31,8 @@ const appRoutes: Routes = [
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
